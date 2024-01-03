3cc395c65b98a80aa333ee0653b2e0713a75bd57
robin maan <robinmaan8055@gmail.com>
2024-01-03 18:11:50 +0530

    making a skeleton for the data

--- components/table/TableWrapper.tsx
+++ components/table/TableWrapper.tsx
@@ -1,13 +1,13 @@
 import FileType from "@/typing"
 import { Button } from "../ui/button"
+import { DataTable } from "./Tables"
+import { columns } from "./columns"
+function TableWrapper({skeletonsFiles}:{skeletonsFiles:FileType[]}) {
 
-
-function TableWrapper(
-    {skeletonsFiles}: {skeletonsFiles: FileType[]}
-) {
   return (
     <div>
-      <Button> Sort by ..</Button>
+     <Button>Sortby..</Button>
+     <DataTable columns={columns} data={skeletonsFiles}></DataTable>
     </div>
   )
 }
--- components/table/columns.tsx
+++ components/table/columns.tsx
@@ -5,7 +5,8 @@ import FileType from "@/typing"
 import { ColumnDef } from "@tanstack/react-table"
 
 import prettyBytes from "pretty-bytes"
-import {FileIcon, defaultStyles} from "react-file-icon"
+import {FileIcon} from "react-file-icon"
+import { defaultStyles } from "react-file-icon"
 import { COLOR_EXTENTION_MAP } from "@/contant"
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -17,16 +18,19 @@ export const columns: ColumnDef<FileType>[] = [
     header:"type",
     cell:({renderValue, ...props}) => {
       const type = renderValue() as string;
-      const extension:string = type.split("/")[1];
+      const extension: string = type.split("/")[1];
+    
       return(
-        <div>
+        <div className="w-10">
           <FileIcon          
           extension={extension}
           labelColor={COLOR_EXTENTION_MAP[extension]}
+          //dfdssd
           
            />
+           
         </div>
-      )
+      );
     },
   },
   {
@@ -45,18 +49,20 @@ export const columns: ColumnDef<FileType>[] = [
     }
   },
   {
-    accessorKey:"downloadURl",
-    header:"Link",
-    cell:({renderValue, ...props})=>{
-      <a 
-      href={renderValue() as string}
-      target="_blank"
-      className="underlinetect-blue-500 hover:text-blue-600"
+  accessorKey: "downloadURL",
+  header: "Link",
+  cell: ({ renderValue, ...props }) => {
+    return (
+      <a
+        href={renderValue() as string}
+        target="_blank"
+        className="underline text-blue-500 hover:text-blue-600"
       >
         Download
       </a>
-    }
-  }
-  
+    );
+  },
+}
 ]
+
 
